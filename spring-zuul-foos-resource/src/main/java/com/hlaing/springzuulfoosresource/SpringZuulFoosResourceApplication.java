bfa07229de7d47c10fc54e0e496313f349b87a25
hlaingwintun <sv.hlaingwt@gmail.com>
2017-12-20 23:42:01 +0800

    init commit for api

---
+++ spring-zuul-foos-resource/src/main/java/com/hlaing/springzuulfoosresource/SpringZuulFoosResourceApplication.java
@@ -0,0 +1,12 @@
+package com.hlaing.springzuulfoosresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZuulFoosResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZuulFoosResourceApplication.class, args);
+	}
+}
---
+++ spring-zuul-foos-resource/src/test/java/com/hlaing/springzuulfoosresource/SpringZuulFoosResourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hlaing.springzuulfoosresource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringZuulFoosResourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
